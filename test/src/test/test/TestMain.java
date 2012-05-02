06774e8855f683f43daa318e040d4908260b45c5
takahashi.y <takahashi.y@IP-0221>
2012-05-02 14:37:46 +0900

    テストクラス実装

---
+++ test/src/test/test/TestMain.java
@@ -0,0 +1,15 @@
+package test.test;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("test");
+
+	}
+
+}

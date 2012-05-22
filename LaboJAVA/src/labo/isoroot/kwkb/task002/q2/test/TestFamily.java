06ab62455aa258fa4d752dc41e46376762dd0147
Yuta Takahashi <m1205123ing@gmail.com>
2012-05-22 18:16:49 +0900

    テストコード作成

---
+++ LaboJAVA/src/labo/isoroot/kwkb/task002/q2/test/TestFamily.java
@@ -0,0 +1,14 @@
+package labo.isoroot.kwkb.task002.q2.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFamily {
+
+    @Test
+    public void test() {
+        fail("まだ実装されていません");
+    }
+
+}

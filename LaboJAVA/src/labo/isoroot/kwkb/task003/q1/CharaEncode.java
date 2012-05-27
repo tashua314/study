e9fa45b173083383da68f55c7bf39e95e4bd9316
tashua314 <m1205123ing@gmail.com>
2012-05-28 07:40:56 +0900

    ファイル出力追加

--- LaboJAVA/src/labo/isoroot/kwkb/task003/q1/CharaEncode.java
+++ LaboJAVA/src/labo/isoroot/kwkb/task003/q1/CharaEncode.java
@@ -1,12 +1,19 @@
 /**
- *
+ * UTF-8で書かれたテキストファイルがあります。
+ * そのファイルをShiftJIS 形式へ変換しコピーしなさい。
+ * ※元となるUTF-8ファイルは添付を利用すること。
  */
 package labo.isoroot.kwkb.task003.q1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.sound.sampled.AudioFormat.Encoding;
 
 /**
  * @author takahashi.y
@@ -14,31 +21,45 @@ import java.io.IOException;
  */
 public class CharaEncode {
 
-    public void read(String filePath) {
-        File file = new File(filePath);
-        BufferedReader br = null;
+	public void encode(String filePath, String copyFilePath) {
+		File file = new File(filePath);
+		BufferedReader br = null;
 
-        try {
-            br = new BufferedReader(new FileReader(file));
-            String line;
-            StringBuffer lines = new StringBuffer();
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line;
+			StringBuffer lines = new StringBuffer();
 
-            // ファイル読み込み
-            while ((line = br.readLine()) != null) {
-                lines.append(line);
-                lines.append(System.getProperty("line.separator"));
-            }
-            System.out.println(lines);
-            String newLine = new String(lines.toString().getBytes("Shift-JIS"), "Shift-JIS");
-            System.out.println(newLine);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+			// ファイル読み込み
+			while ((line = br.readLine()) != null) {
+				lines.append(line);
+				lines.append(System.getProperty("line.separator"));
+			}
+
+			String newLine = new String(lines.toString().getBytes("UTF8"),
+					"Shift-JIS");
+
+			makeFile(copyFilePath, newLine);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void makeFile(String file_name, String data) throws IOException {
+
+		File file = new File(file_name);
+		PrintWriter pw = new PrintWriter(new BufferedWriter(
+				new FileWriter(file)));
+
+		pw.print(data);
+
+		pw.close();
+	}
 }

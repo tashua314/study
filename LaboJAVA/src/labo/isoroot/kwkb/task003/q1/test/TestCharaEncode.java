3342bf3b87b6bd6860800a82528d3353a381e53f
tashua314 <m1205123ing@gmail.com>
2012-05-30 00:54:19 +0900

    テストクラスのコメントとパスの変更

--- LaboJAVA/src/labo/isoroot/kwkb/task003/q1/test/TestCharaEncode.java
+++ LaboJAVA/src/labo/isoroot/kwkb/task003/q1/test/TestCharaEncode.java
@@ -1,24 +1,22 @@
 /**
- *
+ * 課題003に関するテスト
  */
 package labo.isoroot.kwkb.task003.q1.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import labo.isoroot.kwkb.task003.q1.CharaEncode;
 
 import org.junit.Test;
 
 /**
- *
- * @author DELL
- *
+ * CharaEncodeクラスのテスト
  */
 public class TestCharaEncode {
 
-	@Test
+    @Test
     public void testRead() {
         CharaEncode ce = new CharaEncode();
-        ce.encode("C:/Users/DELL/git/study/LaboJAVA/data/utf8.txt", "C:/Users/DELL/git/study/LaboJAVA/data/shift-JIS.txt");
+        ce.encode("./data/", "utf8.txt", "shift-JIS.txt");
         assertEquals(1, 1);
     }
 

8b47bb96fc1749599ea36aa8b55ee3f8050120fe
yuta takahashi <m1205123ing@gmail.com>
2013-06-19 17:15:26 +0900

    ファイル入出力作成

--- FileInOut/src/jp/isoroot/Main.java
+++ FileInOut/src/jp/isoroot/Main.java
@@ -1,13 +1,51 @@
 package jp.isoroot;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
+		// 読み込みファイル
+		String inFile = args[0];
+		// 書き込みファイル
+		String outFile = args[1];
+		// ファイルデータ
+		ArrayList<String> list = new ArrayList<String>();
+
+		try {
+			// ファイルデータセット
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					new FileInputStream(inFile)));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				list.add(line);
+			}
+
+			// ファイル書き込み
+			File file = new File(outFile);
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(
+					file)));
+			for (int i = 0; i < list.size(); i++) {
+				pw.println(list.get(i));
+			}
+			br.close();
+			pw.close();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
